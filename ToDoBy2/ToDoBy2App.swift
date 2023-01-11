d64f11cc429698ef30ff51a98059c12ed9aa67d3
uenot <tobyueno@gmail.com>
2023-01-10 17:11:30 -0800

    Initial Commit

---
+++ ToDoBy2/ToDoBy2App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoBy2App.swift
+//  ToDoBy2
+//
+//  Created by Toby Ueno on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoBy2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
